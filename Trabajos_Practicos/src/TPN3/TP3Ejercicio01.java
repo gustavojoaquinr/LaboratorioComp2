3a9a125f5e2ff046fd3a8447bdb4c1b2146b69a7
gustavojoaquinr <53922367+gustavojoaquinr@users.noreply.github.com>
2019-08-22 16:21:10 -0300

    TPN3 ejer 1 y 3 mejorados

--- Trabajos_Practicos/src/TPN3/TP3Ejercicio01.java
+++ Trabajos_Practicos/src/TPN3/TP3Ejercicio01.java
@@ -1,18 +1,20 @@
 package TPN3;
 
+import java.util.Random;
+
 public class TP3Ejercicio01 {
     public static void main(String[] args) {
 
-        int[] array = new int[100];
+        int[] arreglo = new int[100];
 
-        //Cargar el array utilizando metodo random() de la clase Math
-        for(int i=0; i<100; i++) {
-            array[i]=(int)(Math.random()*100);
+        //Cargar el array utilizando numeros aleatorios sin repeticiones
+        Random randomizer = new Random(System.nanoTime());
+        for(int i=0; i < arreglo.length; i++) {
+            arreglo[i]= randomizer.nextInt(arreglo.length);
         }
 
-        //Imprimir el array
-        System.out.println("Array con numeros aleatorios entre 1 y 100:");
-        for(int elemento:array) {
+        //Mostrar el array
+        for(int elemento:arreglo) {
             System.out.print(elemento+" ");
         }
     }
--- Trabajos_Practicos/src/TPN3/TP3Ejercicio03.java
+++ Trabajos_Practicos/src/TPN3/TP3Ejercicio03.java
@@ -3,40 +3,42 @@ package TPN3;
 import java.util.Scanner;
 
 public class TP3Ejercicio03 {
-
     public static void main(String[] args) {
+
         Scanner s = new Scanner(System.in);
 
-        String nombre="";
-        int dni;
-        int edad;
+        String [][] Personas = new String[3][3];
 
-        TP3Ejercicio03_Personas p[][] = new TP3Ejercicio03_Personas[2][2];
-
-        for(int filas=0;filas<2; filas++){
-            for (int col=0;col<2;col++){
-                System.out.println("Ingresa el nombre");
-                nombre = s.nextLine();
-                System.out.println("Ingresa el dni");
-                dni = s.nextInt();
-                System.out.println("Ingresa la edad");
-                edad = s.nextInt();
-                System.out.println(" ");
-                s.nextLine();
-
-                TP3Ejercicio03_Personas Personas = new TP3Ejercicio03_Personas(nombre,dni,edad);
-                p[filas][col] = Personas;
-            }
-            //p[i] = new TP3Ejercicio03_Personas(nombre,dni,edad);
-        }
-
-        for(int filas=0;filas<2; filas++) {
-            for (int col = 0; col < 2; col++) {
-                System.out.println("NOMBRE: "+p[filas][col].getNombre());
-                System.out.println("DNI: "+p[filas][col].getDni());
-                System.out.println("EDAD: "+p[filas][col].getEdad());
-                System.out.println("");
+        //Cargar la matriz
+        for (int i=0; i<3; i++){
+            System.out.println("Ingrese Persona numero "+ (i+1));
+            System.out.println("(Nombre - Dni - Edad):");
+            for (int j=0; j<3; j++){
+                Personas[j][i] = s.next();
             }
         }
+
+        //Ordenamiento por orden alfabetico
+        for (int x=0;   x<3-1;    x++){
+            for (int y=x+1;   y<3;    y++){
+                if (Personas[0][x].compareTo(Personas[0][y])>0){
+                    for (int z=0; z<3; z++){
+                        String aux = Personas[z][x];
+                        Personas[z][x] = Personas[z][y];
+                        Personas[z][y] = aux;
+                    }
+                }
+            }
+        }
+
+        System.out.println(" ");
+        //Mostrar la matriz
+        for (String[] Fila:Personas) {
+            for (String Elemento:Fila) {
+                System.out.print(" ¦" +Elemento+ "¦\t\t\t\t");
+            }
+            System.out.println(" ");
+        }
+
     }
 }
