56340fff4d2f2352dd5cf9412c7fa7cf7e42934b
gustavojoaquinr <53922367+gustavojoaquinr@users.noreply.github.com>
2019-10-12 13:14:40 -0300

    TP9

--- Trabajos_Practicos/src/TPN9/Ejercicio1/Menu.java
+++ Trabajos_Practicos/src/TPN9/Ejercicio1/Menu.java
@@ -3,69 +3,265 @@ package TPN9.Ejercicio1;
 import java.util.Scanner;
 
 public class Menu {
-    public static void sumaNat(Numero[] nat){
-    int sumaNaturales=0;
-
+    public static void sumaNaturales(Numero[] nat){
+        int sumaNat=0;
         for (int x=0; x < nat.length; x++){
-        if (nat[x].getTipo().equals("natural")){
-            sumaNaturales+=nat[x].getValor1();
+            if (nat[x].getTipo().equals("natural")){
+            sumaNat+=nat[x].getValor1();
         }
     }
-        System.out.println("Suma de numeros naturales: "+sumaNaturales+"\n");
-}
-    public static void sumaComp(Numero[] com){
-        float sumaComplejos1=0, sumaComplejos2=0;
+        System.out.println("Suma de numeros naturales: "+sumaNat+"\n");
+    }
+
+    public static void sumaComplejos(Numero[] com){
+        float sumaCom=0, sumaComp=0;
         for (int x=0; x < com.length; x++){
             if (com[x].getTipo().equals("complejo")){
-                sumaComplejos1+=com[x].getValor1();
-                sumaComplejos2+=com[x].getValor2();
+                sumaCom+=com[x].getValor1();
+                sumaComp+=com[x].getValor2();
             }
         }
-        System.out.println("Suma de todos los numeros complejos: "+sumaComplejos1+", "+sumaComplejos2+"\n");
+        System.out.println("Suma de todos los numeros complejos: "+sumaCom+", "+sumaComp+"\n");
     }
 
     public static void listarNaturales(Numero[] nat){
-        Numero[] operar= nat;
-        int contadorN=0;
-        for (int x = 0;x<operar.length;x++){
-            if (operar[x].getTipo().equals("natural")){
-                contadorN=x;
-                for (int y=x;y<operar.length;y++){
-                    if (operar[x].getTipo().equals("natural")){
-                        if (operar[x].getValor1() > operar[y].getValor1() && x!=y){
-                            Numero burbuja = operar[x];
-                            operar[x]=operar[y];
-                            operar[y]=burbuja;
+        Numero[] op=nat;
+        int cont=0;
+        for (int x = 0;x<op.length;x++){
+            if (op[x].getTipo().equals("natural")){
+                cont=x;
+                for (int y=x;y<op.length;y++){
+                    if (op[x].getTipo().equals("natural")){
+                        if (op[x].getValor1() > op[y].getValor1() && x!=y){
+                            Numero burbuja = op[x];
+                            op[x]=op[y];
+                            op[y]=burbuja;
                         }
                     }
                 }
             }
         }
-        System.out.println("\nLos numeros naturales ordenados son: ");
-        for (int x=0;x<operar.length;x++) {
-            if (operar[x].getTipo().equals("natural") && x==contadorN) {
-                System.out.print((int)(operar[x].getValor1()));
-            }else if (operar[x].getTipo().equals("natural")){
-                System.out.print((int)(operar[x].getValor1())+" , ");
+        System.out.println("\nNumeros naturales ordenados: ");
+        for (int x=0;x<op.length;x++) {
+            if (op[x].getTipo().equals("natural") && x==cont) {
+                System.out.print((int)(op[x].getValor1()));
+            }else if (op[x].getTipo().equals("natural")){
+                System.out.print((int)(op[x].getValor1())+" , ");
             }
 
         }
     }
+
     public static void listarComplejos(Numero[] com){
-        int contadorC=0;
-        System.out.println("\nLos numeros complejos son: ");
+        int cont2=0;
+        System.out.println("\nNumeros complejos: ");
         for (int x=0;x<com.length;x++){
             if (com[x].getTipo().equals("complejo")) {
-                contadorC=x;
+                cont2=x;
             }
         }
         for (int x=0;x<com.length;x++) {
-            if (com[x].getTipo().equals("complejo") && contadorC==x) {
+            if (com[x].getTipo().equals("complejo") && cont2==x) {
                 System.out.print(+com[x].getValor1()+", "+com[x].getValor2());
             }else if (com[x].getTipo().equals("complejo")){
                 System.out.print(+com[x].getValor1()+", "+com[x].getValor2());
             }
 
         }
+    }
+
+    public static void Opciones (Numero [] array){
+        Scanner sc = new Scanner(System.in);
+        int opc=0;
+        do {
+            System.out.println("\nQue desea realizar? 1-Suma de todos los números naturales\n" +
+                    "2-Suma de todos los números complejos\n" +
+                    "3-Listado de número naturales ordenados de menor a mayor separados por una coma\n" +
+                    "4-Listado de los números complejos\n" + "5-Salir");
+            opc = sc.nextInt();
+            switch (opc) {
+                case 1:
+                    Menu.sumaNaturales(array);break;
+                case 2:
+                    Menu.sumaComplejos(array);break;
+                case 3:
+                    Menu.listarNaturales(array);break;
+                case 4:
+                    Menu.listarComplejos(array);break;
+                default: break;
+            }
+        } while (opc!=5);
+    }
+
+    public static void Operaciones(Numero[] array){
+        Scanner sc = new Scanner(System.in);
+        int opc=0;
+        do {
+            System.out.println("\nDesea realizar alguna operacion entre objetos? 1-Suma 2-Resta 3-Multiplicacion 4-NO");
+            opc = sc.nextInt();
+            switch (opc) {
+                case 1:
+                    System.out.println("Que desea sumar? 1-Naturales 2-Complejos");
+                    int x = sc.nextInt();
+                    if (x==1){
+                        Menu.listarNaturales(array);
+                        System.out.println("\nIngrese que numeros naturales quiere sumar:");
+                        System.out.println("Ingrese el primero numero: ");
+                        int n1 = sc.nextInt();
+                        System.out.println("Ingrese el segundo numero: ");
+                        int n2 = sc.nextInt();
+                        boolean bln = false;
+                        for (int z = 0; z < array.length; z++) {
+                            if (array[z].getValor1() == ((double) n1) && array[z].getTipo().equals("natural")) {
+                                for (int i = 0; i < array.length; i++) {
+                                    if (array[i].getValor1() == ((double) n2) && array[i].getTipo().equals("natural")) {
+                                        ((Natural) array[z]).sumar(array[i]);
+                                        bln = true;break;
+                                    }
+                                }
+                            }
+                        }
+                        if (!bln) {
+                            System.out.println("Error");
+                        }
+
+                    } else if (x==2){
+                        Menu.listarComplejos(array);
+                        System.out.println("\nIngrese que numeros complejos quiere sumar");
+                        System.out.println("Ingrese el valor real: ");
+                        double n1 = sc.nextInt();
+                        System.out.println("Ingrese el valor imaginario: ");
+                        double n2 = sc.nextDouble();
+                        System.out.println("Ingrese el valor real del segundo complejo: ");
+                        double n3 = sc.nextDouble();
+                        System.out.println("Ingrese el valor imaginario del segundo complejo: ");
+                        double n4 = sc.nextDouble();
+                        boolean bln = false;
+                        for (int z = 0; z < array.length; z++) {
+                            if (array[z].getValor1() == n1 && array[z].getValor2() == n2 && array[z].getTipo().equals("complejo")) {
+                                for (int i = 0; i < array.length; i++) {
+                                    if (array[i].getValor1() == n3 && array[i].getValor2() == n4 && array[i].getTipo().equals("complejo")) {
+                                        ((Complejo) array[z]).sumar(array[i]);
+                                        bln = true;break;
+                                    }
+                                }
+                            }
+                        }
+                        if (!bln){
+                            System.out.println("Error");
+                        }
+                    }else {
+                        System.out.println("Error, ingrese nuevamente");
+                    }
+                    break;
+                case 2:
+                    System.out.println("Que desea restar? 1-Naturales 2-Complejos");
+                    x = sc.nextInt();
+                    if (x==1){
+                        Menu.listarNaturales(array);
+                        System.out.println("\nIngrese el primero: ");
+                        int n1 = sc.nextInt();
+                        System.out.println("Ingrese el segundo: ");
+                        int n2 = sc.nextInt();
+                        boolean bln = false;
+                        for (int z = 0; z <array.length; z++) {
+                            if (array[z].getValor1() == ((double) n1) && array[z].getTipo().equals("natural")) {
+                                for (int i = 0; i <array.length; i++) {
+                                    if (array[i].getValor1() == ((double) n2) && array[i].getTipo().equals("natural")) {
+                                        ((Natural) array[z]).restar(array[i]);
+                                        bln = true;break;
+                                    }
+                                }
+                            }
+                        }
+                        if (!bln) {
+                            System.out.println("Error");
+                        }
+
+                    } else if (x==2) {
+                        Menu.listarComplejos(array);
+                        System.out.println("\nIngrese el valor real: ");
+                        double n1 = sc.nextInt();
+                        System.out.println("Ingrese el valor imaginario: ");
+                        double n2 = sc.nextDouble();
+                        System.out.println("Ingrese el valor real del segundo complejo: ");
+                        double n3 = sc.nextDouble();
+                        System.out.println("Ingrese el valor imaginario del segundo complejo: ");
+                        double n4 = sc.nextDouble();
+                        boolean bln = false;
+                        for (int z = 0; z <array.length; z++) {
+                            if (array[z].getValor1() == n1 && array[z].getValor2() == n2 && array[z].getTipo().equals("complejo")) {
+                                for (int i = 0; i < array.length; i++) {
+                                    if (array[i].getValor1() == n3 && array[i].getValor2() == n4 && array[i].getTipo().equals("complejo")) {
+                                        ((Complejo) array[z]).restar(array[i]);
+                                        bln = true;break;
+                                    }
+                                }
+                            }
+                        }
+                        if (!bln) {
+                            System.out.println("Error");
+                        }
+                    } else {
+                        System.out.println("Error, ingreso mal");
+                    }
+                    break;
+                case 3:
+                    System.out.println("\nQue desea multiplicar? 1-Naturales 2-Complejos");
+                    x = sc.nextInt();
+                    if (x==1){
+                        Menu.listarNaturales(array);
+                        System.out.println("\nIngrese el primero: ");
+                        int n1 = sc.nextInt();
+                        System.out.println("Ingrese el segundo: ");
+                        int n2 = sc.nextInt();
+
+                        boolean bln = false;
+                        for (int z = 0; z < array.length; z++) {
+                            if (array[z].getValor1() == ((double) n1) && array[z].getTipo().equals("natural")) {
+                                for (int i = 0; i <array.length; i++) {
+                                    if (array[i].getValor1() == ((double) n2) && array[i].getTipo().equals("natural")) {
+                                        ((Natural) array[z]).multiplicar(array[i]);
+                                        bln = true;break;
+                                    }
+                                }
+                            }
+                        }
+                        if (!bln) {
+                            System.out.println("Error");
+                        }
+
+                    } else if (x==2){
+                        Menu.listarComplejos(array);
+                        System.out.println("\nIngrese el valor real: ");
+                        double n1 = sc.nextInt();
+                        System.out.println("Ingrese el valor imaginario: ");
+                        double n2 = sc.nextDouble();
+                        System.out.println("Ingrese el valor real del segundo complejo: ");
+                        double n3 = sc.nextDouble();
+                        System.out.println("Ingrese el valor imaginario del segundo complejo: ");
+                        double n4 = sc.nextDouble();
+                        boolean bln = false;
+                        for (int z = 0; z <array.length; z++) {
+                            if (array[x].getValor1() == n1 && array[z].getValor2() == n2 && array[z].getTipo().equals("complejo")) {
+                                for (int i = 0; i <array.length; i++) {
+                                    if (array[i].getValor1() == n3 && array[i].getValor2() == n4 && array[i].getTipo().equals("complejo")) {
+                                        ((Complejo) array[z]).multiplicar(array[i]);
+                                        bln = true;break;
+                                    }
+                                }
+                            }
+                        }
+                        if (!bln) {
+                            System.out.println("Error");
+                        }
+                    } else {
+                        System.out.println("Error, no ingreso correctamente");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }while(opc!=4);
     }
 }
--- Trabajos_Practicos/src/TPN9/Ejercicio2/Main.java
+++ Trabajos_Practicos/src/TPN9/Ejercicio2/Main.java
@@ -6,18 +6,18 @@ import java.util.Iterator;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Edificio> lista = new ArrayList<Edificio>();
-        Edificio pd1 = new Polideportivo("San Martin",1,2.0445, 8);
-        Edificio pd2 = new Polideportivo("Belgrano",2,2.4585,9);
-        Edificio pd3 = new Polideportivo("Peron", 3,1.2596,15);
-        Edificio of1 = new Oficinas(4,500);
-        Edificio of2 = new Oficinas(2,400);
-        Edificio of3 = new Oficinas(6,350);
-        lista.add(pd1);
-        lista.add(pd2);
-        lista.add(pd3);
-        lista.add(of1);
-        lista.add(of2);
-        lista.add(of3);
+        Edificio p1 = new Polideportivo("San Martin",1,2.0445, 8);
+        Edificio p2 = new Polideportivo("Belgrano",2,2.4585,9);
+        Edificio p3 = new Polideportivo("Peron", 3,1.2596,15);
+        Edificio o1 = new Oficinas(4,500);
+        Edificio o2 = new Oficinas(2,400);
+        Edificio o3 = new Oficinas(6,350);
+        lista.add(p1);
+        lista.add(p2);
+        lista.add(p3);
+        lista.add(o1);
+        lista.add(o2);
+        lista.add(o3);
         Iterator<Edificio> it = lista.iterator();
         while(it.hasNext()){
             System.out.println(it.next());
